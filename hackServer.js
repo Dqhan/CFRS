c1eefe918093091b5476eb0136a305750c11bfdc
dqhan <291927006@qq.com>
2021-04-30 23:50:51 +0800

    init

---
+++ hackServer.js
@@ -0,0 +1,38 @@
+var express = require("express");
+var path = require("path");
+var router = express.Router();
+var cookie = require("cookie-parser");
+var bodyParser = require("body-parser");
+var app = express();
+
+app.use("/", express.static(__dirname));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.set("trust proxy", 1);
+app.use(cookie("express_cookie"));
+
+app.post("/login", function(req, res) {
+  User.findOne({
+    username: req.body.username
+  })
+    .then(function(userInfo) {
+      if (!userInfo) {
+        console.log("user is not exist.");
+        return;
+      }
+      var data = {};
+      data["username"] = userInfo.username;
+      data["password"] = userInfo.password;
+      res.cookie("username", JSON.stringify(data), {
+        maxAge: 900000,
+        httpOnly: true
+      });
+      res.status(200).json(data);
+    })
+    .catch(function(e) {
+      console.log(e);
+    });
+});
+
+app.listen(8889, () => console.log("listening on port 8889."));
+
+module.exports = app;
---
+++ normalScript.js
@@ -0,0 +1,79 @@
+window.onload = function() {
+  (function() {
+    fetch("./userInfo", {
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded",
+        Accept: "application/json",
+        Authorization: localStorage.getItem("token")
+      },
+      method: "GET"
+    })
+      .then(function(res) {
+        return res.json();
+      })
+      .then(function(res) {
+        var a = res;
+      })
+      .catch(function(e) {
+        console.log(e);
+      });
+  })();
+
+  function assemble(data) {
+    let ret = "";
+    for (let it in data) {
+      ret += encodeURIComponent(it) + "=" + encodeURIComponent(data[it]) + "&";
+    }
+    return ret;
+  }
+
+  var registerBtn = document.getElementById("register");
+  registerBtn.addEventListener("click", function() {
+    var xhr = new XMLHttpRequest();
+    var url = "./register";
+    var request = {};
+    request["username"] = document.getElementById(
+      "register-user-name-input"
+    ).value;
+    request["password"] = document.getElementById(
+      "register-password-input"
+    ).value;
+    xhr.open("POST", url, true);
+    xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+    xhr.onreadystatechange = function() {
+      if (xhr.readyState === 4) {
+        if ((xhr.status >= 200 && xhr.status < 300) || xhr.status === 304) {
+          var str = JSON.parse(xhr.responseText).test;
+          var node = `${str}`;
+          document.body.insertAdjacentHTML("beforeend", node);
+        } else {
+          console.log("error", xhr.responseText);
+        }
+      }
+    };
+    xhr.send(assemble(request));
+  });
+  var loginBtn = document.getElementById("login");
+  loginBtn.addEventListener("click", function() {
+    var request = {};
+    request["username"] = document.getElementById("user-name-input").value;
+    request["password"] = document.getElementById("password-input").value;
+    fetch("./login", {
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded",
+        Accept: "application/json"
+      },
+      method: "POST",
+      body: assemble(request)
+    })
+      .then(function(res) {
+        return res.json();
+      })
+      .then(function(res) {
+        localStorage.setItem("token", res.token);
+      })
+      .catch(function(e) {
+        console.log(e);
+      });
+  });
+};
